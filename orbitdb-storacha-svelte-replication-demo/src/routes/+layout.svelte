c51a1729e15568e9f54cda916d939045ac4820fd
Nico Krause <post@nicokrause.com>
2025-09-28 12:19:07 +0200

    adding a storacha backup and restore example where Alice & Bob is connected and replicating data via two Helia(IPFS)/OrbitDB nodes running in the same browser connected via a public relay

---
+++ orbitdb-storacha-svelte-replication-demo/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.css";
+  import { theme } from "$lib/theme.js";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    theme.init();
+  });
+</script>
+
+<div class="app">
+  <slot />
+</div>
